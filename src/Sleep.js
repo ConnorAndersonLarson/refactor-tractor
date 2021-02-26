04a9c811345a7641e097d43c18ccab487010e436
Elizabeth Hahn <elizhahn87@gmail.com>
2021-02-25 22:54:16 -0700

    Move sleep methods to sleep class and refactor updateSleep

--- src/Sleep.js
+++ src/Sleep.js
@@ -1,16 +1,57 @@
-class Sleep { // instance for the user's sleep each day
-  constructor(data, userRepository) {
-    this.userId = data.userID;
-    this.date = data.date;
-    this.hoursSlept = data.hoursSlept;
-    this.sleepQuality = data.sleepQuality;
-    this.sleep(userRepository);
+class Sleep { 
+  constructor(todayData, allTimeData) {
+    this.userID = todayData.userID;
+    this.date = todayData.date;
+    this.hoursSlept = todayData.hoursSlept; 
+    this.sleepQuality = todayData.sleepQuality; 
+    this.hoursSleptAverage = 0;
+    this.sleepQualityAverage = 0;
+    this.sleepHoursRecord = [];
+    this.sleepQualityRecord = [];
+    this.updateSleep(allTimeData); 
   }
-  sleep(userRepo) {
-    var sleep = this;
-    userRepo.users.find(function(user) {
-      return user.id === sleep.userId;
-    }).updateSleep(this.date, this.hoursSlept, this.sleepQuality);
+ 
+  updateSleep(sleepData) {
+     sleepData.forEach(sleepItem => {
+      this.sleepHoursRecord.unshift({
+        'date': sleepItem.date,
+        'hours': sleepItem.hoursSlept
+      });
+      this.sleepQualityRecord.unshift({
+        'date': sleepItem.date,
+        'quality': sleepItem.sleepQuality
+      });
+    })
+  }
+  calcAverageHoursSlept() {  
+    this.hoursSleptAverage = this.reduce((averageHours, day) => {
+        return ((averageHours += day.hours)/this.sleepHoursRecord.length).toFixed(1);
+    }, 0)
+  }
+
+  calcAverageSleepQuality() {  
+    this.sleepQualityAverage = this.sleepQualityRecord.reduce((averageQuality, day) => {
+      return ((averageQuality += day.quality)/this.sleepQualityRecord.length).toFixed(1);
+  }, 0)
+}
+   
+  calculateAverageHoursThisWeek(todayDate) {
+    return (this.sleepHoursRecord.reduce((sum, sleepAct) => {
+      let index = this.sleepHoursRecord.indexOf(this.sleepHoursRecord.find(sleep => sleep.date === todayDate));
+      if (index <= this.sleepHoursRecord.indexOf(sleepAct) && this.sleepHoursRecord.indexOf(sleepAct) <= (index + 6)) {
+        sum += sleepAct.hours;
+      }
+      return sum;
+    }, 0) / 7).toFixed(1);
+  }
+  calculateAverageQualityThisWeek(todayDate) {
+    return (this.sleepQualityRecord.reduce((sum, sleepAct) => {
+      let index = this.sleepQualityRecord.indexOf(this.sleepQualityRecord.find(sleep => sleep.date === todayDate));
+      if (index <= this.sleepQualityRecord.indexOf(sleepAct) && this.sleepQualityRecord.indexOf(sleepAct) <= (index + 6)) {
+        sum += sleepAct.quality;
+      }
+      return sum;
+    }, 0) / 7).toFixed(1);
   }
 }
 
