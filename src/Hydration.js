d4ef6cc0c85e50f52799552619ee1df47edbc9a3
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-02-27 07:03:19 -0600

    Refactor tests to check new hydration class structure

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,9 +1,9 @@
 
 class Hydration {
   constructor(userID, date) {
-    this.userID = userID;
+    this.userId = userID;
     this.date = date;
-    //this.ounces = data.numOunces;
+    this.totalOunces = 0;
     this.ouncesAverage = 0;
     this.ouncesRecord = [];
     //this.updateHydration();
@@ -19,10 +19,11 @@ class Hydration {
 
   updateHydration(data) {
     this.ouncesRecord = data.reduce((record, dataset) => {
-      if (this.userID === dataset.userID) {
+      if (this.userId === dataset.userID) {
         record.unshift({[dataset.date]: dataset.numOunces})
+        this.totalOunces+=dataset.numOunces
+        this.ouncesAverage+=dataset.numOunces
       }
-      this.ouncesAverage+=dataset.numOunces
       return record;
     }, [])
     this.ouncesAverage = this.ouncesAverage / this.ouncesRecord.length
