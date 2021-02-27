e6ef3e4c1f076e5796deb41f7961bb5856e84f05
Elizabeth Hahn <elizhahn87@gmail.com>
2021-02-27 08:55:26 -0700

    Combine calculate weekly data methods

--- src/Sleep.js
+++ src/Sleep.js
@@ -6,6 +6,8 @@ class Sleep {
     this.sleepQuality = 0; 
     this.averageSlept = 0;
     this.averageQuality = 0;
+    this.weeklySlept = 0; 
+    this.weeklyQuality = 0; 
     this.sleepRecord = []; 
   }
 
@@ -39,50 +41,32 @@ class Sleep {
     } else {
         averages.hours += day.hours; 
         averages.quality += day.quality;
-    }
+    }  
+       return averages
+       
+    }, {})
+    this.averageSlept = Number((averageSleepData.hours/this.sleepRecord.length).toFixed(1));
+    this.averageQuality = Number((averageSleepData.quality/this.sleepRecord.length).toFixed(1));  
+  }
+   
+  calcWeeklyAvgData(date) {
+    const sleepDataIndex = this.sleepRecord.findIndex(sleep => {
+      return sleep.date === date;
+    }); 
+    const lastWeekData = this.sleepRecord.slice(sleepDataIndex, sleepDataIndex + 7);
+    const averageWeeklyData = lastWeekData.reduce((averages, day) => {
+      if(!averages.hours){
+        averages.hours = day.hours; 
+        averages.quality = day.quality;
+    } else {
+        averages.hours += day.hours; 
+        averages.quality += day.quality;
+    }  
        return averages
     }, {})
-    this.averageSlept = (averageSleepData.hours/this.sleepRecord.length).toFixed(1); 
-    this.averageQuality = (averageSleepData.quality/this.sleepRecord.length).toFixed(1);  
+    this.weeklySlept = Number((averageWeeklyData.hours/7).toFixed(1));
+    this.weeklyQuality = Number((averageWeeklyData.quality/7).toFixed(1));
   }
-   
-  calcWeeklyAvgHoursSlept(date) {
-    const sleptHoursIndex = this.sleepRecord.findIndex(sleep => {
-      return sleep.date === date
-    })
-    const lastWeekSleepHours = this.sleepRecord.slice(sleptHoursIndex, 7)
-    const averageSleepHours = lastWeekSleepHours.reduce((sumHours, day) => {
-       return sumHours += day.hours/7
-    }, 0)
-    return averageSleepHours.toFixed(1);    
-  }
-  
-  calcWeeklyAvgQuality(date) {
-    const sleepQualityIndex = this.sleepRecord.findIndex(sleep => {
-      return sleep.date === date;
-    })
-    const lastWeekQualityHours = this.sleepRecord.slice(sleepQualityIndex, 7); 
-    const averageQualityHours = lastWeekQualityHours.reduce((sumHours, day) => {
-      return sumHours += day.hours/7
-   }, 0)
-   
-   return averageQualityHours.toFixed(1);    
-  }
-
-  // calculateAverageQualityThisWeek(todayDate) {
-  //   return (this.sleepQualityRecord.reduce((sum, sleepAct) => {
-  //     let index = this.sleepQualityRecord.indexOf(this.sleepQualityRecord.find(sleep => sleep.date === todayDate));
-  //     if (index <= this.sleepQualityRecord.indexOf(sleepAct) && this.sleepQualityRecord.indexOf(sleepAct) <= (index + 6)) {
-  //       sum += sleepAct.quality;
-  //     }
-  //     return sum;
-  //   }, 0) / 7).toFixed(1);
-  // }
-  // calcWeeklyAvg() {
-  //   const lastWeekSleepHours = this.sleepHoursRecord.slice(0, 7);
-  //   const lastWeekQualityHours = this.sleepQualityRecord.slice(0, 7);
-
-  // }
 }
 
 
