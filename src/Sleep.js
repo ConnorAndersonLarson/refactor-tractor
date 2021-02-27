0a525fecf58f7e2f296cf917fefb7390a44e2c4b
Elizabeth Hahn <elizhahn87@gmail.com>
2021-02-26 19:24:49 -0700

    Added method to find user's current sleep data

--- src/Sleep.js
+++ src/Sleep.js
@@ -1,18 +1,29 @@
 class Sleep { 
-  constructor(todayData, allTimeData) {
-    this.userID = todayData.userID;
-    this.date = todayData.date;
-    this.hoursSlept = todayData.hoursSlept; 
-    this.sleepQuality = todayData.sleepQuality; 
+  constructor(userID, date) {
+    this.userID = userID
+    this.date = date; 
+    this.hoursSlept = 0; 
+    this.sleepQuality = 0; 
     this.hoursSleptAverage = 0;
     this.sleepQualityAverage = 0;
     this.sleepHoursRecord = [];
     this.sleepQualityRecord = [];
-    this.updateSleep(allTimeData); 
   }
+
+  findTodaySleepData(sleepData) {
+    const currentDayData = sleepData.find(dataItem => {
+      return this.userID === dataItem.userID && this.date === dataItem.date
+    })
+    this.hoursSlept = currentDayData.hoursSlept; 
+    this.sleepQuality = currentDayData.sleepQuality; 
+  }
+
  
-  updateSleep(sleepData) {
-     sleepData.forEach(sleepItem => {
+  updateSleepRecords(sleepData) {
+    const userSleepData = sleepData.filter(sleepItem => {
+      return sleepItem.userID === this.userID; 
+    })
+     userSleepData.forEach(sleepItem => {
       this.sleepHoursRecord.unshift({
         'date': sleepItem.date,
         'hours': sleepItem.hoursSlept
@@ -36,6 +47,11 @@ class Sleep {
 }
    
   calculateAverageHoursThisWeek(todayDate) {
+
+    //slice or splice out of sleepHoursRecord array from 0-6, reduce that down baybay
+
+    const lastWeekSleepHours = this.sleepHoursRecord.slice(0, 8)
+    console.log(lastWeekSleepHours)
     return (this.sleepHoursRecord.reduce((sum, sleepAct) => {
       let index = this.sleepHoursRecord.indexOf(this.sleepHoursRecord.find(sleep => sleep.date === todayDate));
       if (index <= this.sleepHoursRecord.indexOf(sleepAct) && this.sleepHoursRecord.indexOf(sleepAct) <= (index + 6)) {
@@ -44,6 +60,7 @@ class Sleep {
       return sum;
     }, 0) / 7).toFixed(1);
   }
+
   calculateAverageQualityThisWeek(todayDate) {
     return (this.sleepQualityRecord.reduce((sum, sleepAct) => {
       let index = this.sleepQualityRecord.indexOf(this.sleepQualityRecord.find(sleep => sleep.date === todayDate));
