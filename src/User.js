558e1a1427695c31e930e2a6946dfa82126a55db
Elizabeth Hahn <elizhahn87@gmail.com>
2021-03-01 22:08:21 -0700

    Create helper methods for inheritance

--- src/User.js
+++ src/User.js
@@ -1,5 +1,5 @@
 import Hydration from './Hydration';
-import Sleep from "./Sleep";
+// import Sleep from "./Sleep";
 
  class User {
   constructor(userData, date) {
@@ -14,7 +14,6 @@ import Sleep from "./Sleep";
     this.accomplishedDays = [];
     this.friendsNames = [];
     this.friendsActivityRecords = []
-    this.sleep = new Sleep(this.id, date);
     this.hydration = new Hydration(this.id, date); 
   }
   getFirstName() {
@@ -22,19 +21,19 @@ import Sleep from "./Sleep";
     return names[0].toUpperCase();
   }
 
-  findClimbingRecord() {
-    return this.activityRecord.sort((a, b) => {
-      return b.flightsOfStairs - a.flightsOfStairs;
-    })[0].flightsOfStairs;
-  }
-  calculateDailyCalories(date) {
-    let totalMinutes = this.activityRecord.filter(activity => {
-      return activity.date === date
-    }).reduce((sumMinutes, activity) => {
-      return sumMinutes += activity.minutesActive
-    }, 0);
-    return Math.round(totalMinutes * 7.6);
-  }
+  // findClimbingRecord() {
+  //   return this.activityRecord.sort((a, b) => {
+  //     return b.flightsOfStairs - a.flightsOfStairs;
+  //   })[0].flightsOfStairs;
+  // }
+  // calculateDailyCalories(date) {
+  //   let totalMinutes = this.activityRecord.filter(activity => {
+  //     return activity.date === date
+  //   }).reduce((sumMinutes, activity) => {
+  //     return sumMinutes += activity.minutesActive
+  //   }, 0);
+  //   return Math.round(totalMinutes * 7.6);
+  // }
 
   findFriendsNames(users) {
     this.friends.forEach(friend => {
@@ -68,6 +67,33 @@ import Sleep from "./Sleep";
       'totalWeeklySteps': this.totalStepsThisWeek
     });
     this.friendsActivityRecords = this.friendsActivityRecords.sort((a, b) => b.totalWeeklySteps - a.totalWeeklySteps);
+  }
+  updateRecord(healthData, record) {
+    const currentData = healthData.filter(healthItem => {
+      return this.id === healthItem.userID;
+    })
+    currentData.forEach(dataItem => {
+     record.unshift(dataItem);
+   });
+  }
+
+  findTodayData(healthData) {
+  const todaysData = healthData.find(healthItem => {
+    return this.id === healthItem.userID && this.date === healthItem.date;
+  }) 
+  return todaysData; 
+  }
+
+  findWeeklyData(date, healthRecord) {
+    const currentDateIndex = healthRecord.findIndex(dataItem => {
+      return dataItem.date === date; 
+    });
+    const currentWeekData = healthRecord.slice(currentDateIndex, currentDateIndex + 7); 
+    return currentWeekData;   
+  }
+  
+  calcAverage(healthData, total, decimalPlace) {
+    return Number((healthData/total).toFixed(decimalPlace));
   }
 }
 
